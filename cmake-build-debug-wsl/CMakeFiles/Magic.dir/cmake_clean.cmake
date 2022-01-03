506c6f1f5aea5d6289627b0d9aa222cf0ae13e20
Bilail <bilail.benyahia@outlook.fr>
2022-01-03 22:13:27 +0100

    1er commit

---
+++ cmake-build-debug-wsl/CMakeFiles/Magic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Magic.dir/main.cpp.o"
+  "Magic"
+  "Magic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Magic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug-wsl/CMakeFiles/Magic.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Magic.
+# This may be replaced when dependencies are built.
